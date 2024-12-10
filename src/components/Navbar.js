df7507db64464fb4323662e71b5a295c39adda77
PendingUsername <beastmodepending@gmail.com>
2024-12-09 21:18:24 -0500

    adding images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link to="/" className="flex items-center">
           <img src="/assets/logo.png" className="h-8 mr-3" alt="Your Agency Logo" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap text-primary">Your Agency</span>
+          <span className="self-center text-xl font-semibold whitespace-nowrap text-primary">JLC WebDev</span>
         </Link>
         <div className="flex items-center lg:order-2">
           <Link to="/contact" className="text-white bg-primary hover:bg-secondary focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 lg:px-5 lg:py-2.5">
